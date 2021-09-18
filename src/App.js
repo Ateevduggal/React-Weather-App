5d09d5a20a18b8e950886a9ed778c6c9b20efdc8
AteevDuggal <akd3257@gmail.com>
2021-09-18 13:32:07 +0530

    Part - 1 -> Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+    const [city, setCity] = useState("")//for input field
+    const [search, setSearch] = useState("");//for the name of the city or country
+    useEffect(() => {
+      const fetchApi = async () =>{
+        const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b78023fc9717be1db9ae7347f66dc760`)
+        const resJ = await res.json();
+        console.log(resJ);
+        setCity(resJ.main)
+      }      
+    fetchApi();
+    },[search]);
+
+  const Entry = (e) => {
+    setSearch(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <div className="row text-center mt-5" id="weather">
+        <div className="col-md-4 col-sm-3"></div>
+        <div className="col-md-3 col-sm-5">
+           <input 
+              type="search" 
+              className="form-control form-control-sm"
+              onChange = {Entry}
+            />
+          <div className="w-100"></div>
+        {
+          !city ? 
+          (
+            <p>Please check the Input</p>
+          ) : (
+            <>
+          <div className="d-flex justify-content-center mt-5">
+          <i className="fas fa-street-view"></i>
+          <h2>{search}</h2>
+          </div>
+          <div className="w-100 mt-5">
+            <h2>{city.temp} &deg;C</h2>
+            <h4 className="temp_min_max">
+              Min: 5.3&deg;C ||   Max: 5.7&deg;C
+            </h4>
+          </div>
+            </>
+          )
+        }
+        </div>
+      </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/CodeWithoutJSX.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CodeWithoutJSX = () => {
+    return React.createElement("div", null, React.createElement("h1", null, "Hello World!"))
+       
+}
+
+export default CodeWithoutJSX;
